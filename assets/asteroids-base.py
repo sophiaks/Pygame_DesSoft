bdceaf6fb30f559326ea1a6a2af7c226bbdb228e
Sophia <sophia.shigueoka@gmail.com>
2019-04-23 17:05:48 -0300

    Update asteroids-base.py

--- assets/asteroids-base.py
+++ assets/asteroids-base.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+## -*- coding: utf-8 -*-
 
 # Importando as bibliotecas necessárias.
 import pygame
@@ -8,9 +8,9 @@ from os import path
 img_dir = path.join(path.dirname(__file__), 'img')
 
 # Dados gerais do jogo.
-WIDTH = 480 # Largura da tela
-HEIGHT = 600 # Altura da tela
-FPS = 60 # Frames por segundo
+WIDTH = 480  # Largura da tela
+HEIGHT = 600  # Altura da tela
+FPS = 60  # Frames por segundo
 
 # Define algumas variáveis com as cores básicas
 WHITE = (255, 255, 255)
@@ -19,6 +19,35 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        player_img = pygame.image.load(
+            path.join(img_dir, "playerShip1_orange.png")).convert()
+        self.image = player_img
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.certerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+
+        self.speedx = 0
+
+        def update(self):
+            self.rect.x += self.speedx
+
+            if self.rect.right > WIDTH:
+                self.rect.right = WIDTH
+            if self.rect.left < 0:
+                self.rect.left = 10
+
+        screen.fill(BLACK)
+        screen.blit(background, background_rect)
+        all_sprites.draw(screen)
+        pygame.displayflip()
+
 
 # Inicialização do Pygame.
 pygame.init()
@@ -30,36 +59,41 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 # Nome do jogo
 pygame.display.set_caption("Asteroids")
 
-# Variável para o ajuste de velocidade
-clock = pygame.time.Clock()
-
 # Carrega o fundo do jogo
 background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
 
 # Comando para evitar travamentos.
 try:
-    
+
     # Loop principal.
     running = True
     while running:
-        
-        # Ajusta a velocidade do jogo.
-        clock.tick(FPS)
-        
+
         # Processa os eventos (mouse, teclado, botão, etc).
         for event in pygame.event.get():
-            
+
             # Verifica se foi fechado
             if event.type == pygame.QUIT:
                 running = False
-    
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.speedx = -8
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 8
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    player.speedx = 0
+                if event.key == pygem.K_RIGHT:
+                    player.speedx = 0
+        all_sprites.update()
+
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
         screen.blit(background, background_rect)
-        
+
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
-        
+
 finally:
     pygame.quit()
